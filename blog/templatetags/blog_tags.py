acd39cbf1073cec13e9e6004bb2c7df8e6cc6a25
ThePoori <mkh.mkh0918@gmail.com>
2026-08-31 23:15:56 +0330

    just read

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,14 +3,17 @@ from blog.models import *
 
 register = template.Library()
 
+# get post count
 @register.simple_tag()
 def total_posts():
     return Post.objects.count()
 
+# get comment count
 @register.simple_tag()
 def total_comments():
     return Comment.objects.filter(active=True).count()
 
+# get last post by date
 @register.simple_tag()
 def last_post_date():
     return Post.published.last().publish
