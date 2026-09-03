b446fe47128cbd0c8a94dc25ff9bacbbc90f1a30
ThePoori <mkh.mkh0918@gmail.com>
2026-09-03 21:44:15 +0330

    read codes

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -21,11 +21,12 @@ def total_comments():
 def last_post_date():
     return Post.published.last().publish
 
+# use annotate in templatetags
 @register.simple_tag
 def most_popular_posts(count=5):
     return Post.published.annotate(comment_count=Count("comments")).order_by("-comment_count")[:count]
 
-
+# use inclusion tag
 @register.inclusion_tag("partials/latest_post.html")
 def latest_posts(count=4):
     l_posts = Post.published.order_by('-publish')[:count]
@@ -34,12 +35,12 @@ def latest_posts(count=4):
     }
     return context
 
-
+# use filter to markdown
 @register.filter(name='markdown')
 def to_markdown(text):
     return mark_safe(markdown(text))
 
-
+# just practice
 @register.simple_tag
 def max_reading_time(count=5):
     return Post.published.aggregate(max_read_time=Max('reading_time'))
