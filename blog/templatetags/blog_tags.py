2c6b9ef5b923a9026680f187e58862b1a12f08bd
ThePoori <mkh.mkh0918@gmail.com>
2026-09-04 21:52:53 +0330

    connect to now database(postgesql)

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,8 +3,11 @@ from blog.models import *
 from django.db.models import Count, Max, Min
 from markdown import markdown
 from django.utils.safestring import mark_safe
+from django.contrib.auth import get_user_model
+import re
 
 register = template.Library()
+User = get_user_model()
 
 # get post count
 @register.simple_tag()
@@ -21,11 +24,20 @@ def total_comments():
 def last_post_date():
     return Post.published.last().publish
 
+# use annotate in templatetags
 @register.simple_tag
 def most_popular_posts(count=5):
     return Post.published.annotate(comment_count=Count("comments")).order_by("-comment_count")[:count]
 
 
+@register.simple_tag
+def most_popular_author(count=5):
+    top_users = User.objects.annotate(post_count=Count("user_posts")).order_by("-post_count")[:count]
+    if top_users:
+        return Post.published.filter(author__in=top_users).select_related("author").order_by('-publish')
+    return Post.published.none()
+
+# use inclusion tag
 @register.inclusion_tag("partials/latest_post.html")
 def latest_posts(count=4):
     l_posts = Post.published.order_by('-publish')[:count]
@@ -34,12 +46,36 @@ def latest_posts(count=4):
     }
     return context
 
-
+# use filter to markdown
 @register.filter(name='markdown')
 def to_markdown(text):
     return mark_safe(markdown(text))
 
 
 @register.simple_tag
-def max_reading_time(count=5):
-    return Post.published.aggregate(max_read_time=Max('reading_time'))
+def max_reading_time():
+    max_time = Post.published.aggregate(max_val=Max('reading_time'))['max_val']
+    if max_time is not None:
+        post = Post.published.filter(reading_time=max_time).first()
+        return post
+    return None
+
+
+@register.simple_tag
+def min_reading_time():
+    min_time = Post.published.aggregate(min_val=Min('reading_time'))['min_val']
+    if min_time is not None:
+        post = Post.published.filter(reading_time=min_time).first()
+        return post
+    return None
+
+
+@register.filter
+def censor(value):
+    if not isinstance(value, str):
+        return value
+    bad_words = ['fuck', 'shet', 'bitch', 'applepulisher']
+    for word in bad_words:
+        pattern = re.compile(re.escape(word), re.IGNORECASE)
+        value = pattern.sub('*' * len(word), value)
+    return value
--- config/settings.py
+++ config/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'blog',
+        "USER": 'blog_admin',
+        "PASSWORD": '123',
+        "HOST": 'localhost',
+        "PORT": '5432',
     }
 }
 
